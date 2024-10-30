b775afb2b67e62aba62515b9dc75fd9b29c6ac46
Gabriel Lievano <git+idris@lievano.me>
2024-10-30 15:50:10 -0700

    Add a product case to CopyFilesElement

--- Sources/XcodeGraph/Models/CopyFileElement.swift
+++ Sources/XcodeGraph/Models/CopyFileElement.swift
@@ -4,6 +4,7 @@ import Path
 public enum CopyFileElement: Equatable, Hashable, Codable, Sendable {
     case file(path: AbsolutePath, condition: PlatformCondition? = nil, codeSignOnCopy: Bool = false)
     case folderReference(path: AbsolutePath, condition: PlatformCondition? = nil, codeSignOnCopy: Bool = false)
+    case product(dependency: TargetDependency, condition: PlatformCondition? = nil, codeSignOnCopy: Bool = false)
 
     public var path: AbsolutePath {
         switch self {
@@ -11,6 +12,12 @@ public enum CopyFileElement: Equatable, Hashable, Codable, Sendable {
             return path
         case let .folderReference(path, _, _):
             return path
+        case let .product(dependency, _, _):
+            do {
+                return try AbsolutePath(validating: "//\(dependency)gosh") // REVIEW: This is going to be a mess.
+            } catch {
+                fatalError("What the fuck are you making me do?")
+            }
         }
     }
 
@@ -20,19 +27,21 @@ public enum CopyFileElement: Equatable, Hashable, Codable, Sendable {
             return false
         case .folderReference:
             return true
+        case .product:
+            return true // REVIEW: What's a reference?
         }
     }
 
     public var condition: PlatformCondition? {
         switch self {
-        case let .file(_, condition, _), let .folderReference(_, condition, _):
+        case let .file(_, condition, _), let .folderReference(_, condition, _), let .product(_, condition, _):
             return condition
         }
     }
 
     public var codeSignOnCopy: Bool {
         switch self {
-        case let .file(_, _, codeSignOnCopy), let .folderReference(_, _, codeSignOnCopy):
+        case let .file(_, _, codeSignOnCopy), let .folderReference(_, _, codeSignOnCopy), let .product(_, _, codeSignOnCopy):
             return codeSignOnCopy
         }
     }
